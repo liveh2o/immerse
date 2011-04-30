a353332f2937b363d0050f424186e33f518dec0d
Adam Hutchison <liveh2o@gmail.com>
2011-04-30 06:09:43 +0000

    Team is now loading

--- wp-content/themes/theme/the-team.php
+++ wp-content/themes/theme/the-team.php
@@ -1,28 +1,21 @@
 <section id="extra" class="section">
 	<h4 id="team-title">The Team</h4>
 	<ul id="team">
-		
-		<?php $f_loop = new WP_Query(); ?>
+		<?php $f_loop = new WP_Query('post_type=team&posts_per_page=-1'); ?>
 		<?php while ($f_loop->have_posts()) : $f_loop->the_post(); ?>	
-			
 		<li>
 			<div class="my-info">
-				
 				<?php if ( has_post_thumbnail() ) {?>
 				<?php the_post_thumbnail('type3'); ?>
-				<?php };?>	
+				<?php };?>
 				<h5><?php the_title(); ?></h5>
-				
 				<?php $person_title = get_post_meta($post->ID, 'person_title', $single = true); ?>
 				<strong><?php echo $person_title; ?></strong>
 			</div>
-			
 			<div class="my-bio">
-				<?php the_excerpt('Read the rest of this entry &raquo;'); ?>
+				<?php the_content(); ?>
 			</div>
 		</li>
-		
 		<?php endwhile; ?>
-		
 	</ul>
 </section>	
