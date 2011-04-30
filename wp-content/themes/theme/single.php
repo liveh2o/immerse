f59ff174984292259e593a227018aad4a5c5842a
Adam Hutchison <liveh2o@gmail.com>
2011-04-30 06:31:15 +0000

    Added some markup for the JS pagination. Added a real FB Like button, and fixed how the badge is selected

--- wp-content/themes/theme/single.php
+++ wp-content/themes/theme/single.php
@@ -6,113 +6,96 @@
 
 get_header(); ?>
 
-	
+    
 
 <?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>		
-		
-<?php $term = get_term_by( 'id', get_query_var( $post->ID ), get_query_var( 'taxonomy' ) ); ?>
-<?php $val = ($term->name); echo $val;?>
+<?php while (have_posts()) : the_post(); ?>     
+        
+<?php $category = get_the_category(); ?>
+<?php $val = ($category[0]->name); ?>
 
 <section id="splash" class="section">
-	<?php include_once 'section-menu-vert.php'; ?>
-	
-	<?php if($val == "Church"){?>
-	<div id="section-image">
-		<img src="<?php bloginfo('template_directory'); ?>/img/article-slide1.jpg" alt="" />
-	</div>	
-	<img src="<?php bloginfo('template_directory'); ?>/img/badge1.png" alt="" id="badge" />
-	<?php } else if($val == "Arts and Culture"){?>
-	<div id="section-image">
-		<img src="<?php bloginfo('template_directory'); ?>/img/article-slide2.jpg" alt="" />
-	</div>	
-	<img src="<?php bloginfo('template_directory'); ?>/img/badge2.png" alt="" id="badge" />	
-	<?php } else if($val == "Story"){?>
-	<div id="section-image">
-		<img src="<?php bloginfo('template_directory'); ?>/img/article-slide3.jpg" alt="" />
-	</div>	
-	<img src="<?php bloginfo('template_directory'); ?>/img/badge3.png" alt="" id="badge" />	
-	<?php } else if($val == "Christian History and Thought"){?>
-	<div id="section-image">
-		<img src="<?php bloginfo('template_directory'); ?>/img/article-slide4.jpg" alt="" />
-	</div>	
-	<img src="<?php bloginfo('template_directory'); ?>/img/badge4.png" alt="" id="badge" />	
-	<?php } else if($val == "Theology"){?>
-	<div id="section-image">
-		<img src="<?php bloginfo('template_directory'); ?>/img/article-slide5.jpg" alt="" />
-	</div>	
-	<img src="<?php bloginfo('template_directory'); ?>/img/badge5.png" alt="" id="badge" />	
-	<?php } else {?>
-	<div id="section-image">
-		<?php the_post_thumbnail( 'full' ); ?>
-	</div>	
-	<img src="<?php bloginfo('template_directory'); ?>/img/badge6.png" alt="" id="badge" />	
-	<?php };?>
+    <?php include_once 'section-menu-vert.php'; ?>
+    <div id="section-image">
+        <?php the_post_thumbnail( 'full' ); ?>
+    </div>  
+    <?php if($val == "Church"){?>
+    <img src="<?php bloginfo('template_directory'); ?>/img/badge1.png" alt="" id="badge" />
+    <?php } else if($val == "Arts and Culture"){?>
+    <img src="<?php bloginfo('template_directory'); ?>/img/badge2.png" alt="" id="badge" /> 
+    <?php } else if($val == "Story"){?>
+    <img src="<?php bloginfo('template_directory'); ?>/img/badge5.png" alt="" id="badge" /> 
+    <?php } else if($val == "Christian History and Thought"){?>
+    <img src="<?php bloginfo('template_directory'); ?>/img/badge3.png" alt="" id="badge" /> 
+    <?php } else if($val == "Theology"){?>
+    <img src="<?php bloginfo('template_directory'); ?>/img/badge6.png" alt="" id="badge" /> 
+    <?php } else {?>
+    <img src="<?php bloginfo('template_directory'); ?>/img/badge4.png" alt="" id="badge" /> 
+    <?php };?>
 </section><!-- e: splash -->
-				
-		
+                
+        
 
 <section id="article-head" class="section">
-	<span class="author">
-		<?php the_author(); ?>
-	</span>
-	<a href="#" class="al1">Past Articles</a>
-	<span class="al2"><?php tweet_this('facebook', '[TITLE] [EXCERPT] [URL]', 'Share This',
-		'Share This [URL]', 'noicon', 'share-this',
-		'tt', 'Post to Facebook'); ?>
-	</span>
-	<span class="al3"><?php tweet_this('twitter', '[TITLE] [EXCERPT] [URL] via @immersejournal', 'Tweet This',
-		'Tweet This [URL]', 'noicon', 'tweet-this',
-		'tt', 'Post to Twitter'); ?>
-	</span>
+    <span class="author">
+        <?php the_author(); ?>
+    </span>
+    <a href="#" class="al1">Past Articles</a>
+    <iframe src="http://www.facebook.com/plugins/like.php?href=<?php the_permalink() ?>&layout=button_count&show_faces=false&width=79&action=like&colorscheme=light" scrolling="no" frameborder="0"  allowTransparency="true" style="overflow:hidden; width:90px; height:29px; float:right; padding:8px 0 0 5px;"></iframe>
+    <div class="al2"><img src="/wp-content/themes/theme/img/like.jpg"></div>
+    <span class="al3"><?php tweet_this('twitter', '[TITLE] [EXCERPT] [URL] via @immersejournal', 'Tweet This',
+        'Tweet This [URL]', 'noicon', 'tweet-this',
+        'tt', 'Post to Twitter'); ?>
+    </span>
 </section>
 
 
 
 <section id="article" class="section">
-	<article class="entry ar-entry">
-	
-		<?php the_content('Read the rest of this entry &raquo;'); ?>
-	</article>
-	
-		
-		<div class="navigation">
-			<span class="alignleft"><?php previous_post('%', 'PREVIOUS', 'no'); ?></span>
-			<span class="alignright"><?php next_post('%', 'NEXT', 'no'); ?></span>
-		</div>
-	
-
+    <div id="page-window">
+        <article class="entry ar-entry paginate">
+            <?php the_content('Read the rest of this entry &raquo;'); ?>
+        </article>
+    </div>
+    <div class="navigation">
+        <span class="alignleft"><a href="#" id="previous_button" style="display:none;">PREVIOUS</a></span>
+        <span class="alignright"><a href="#" id="next_button">NEXT</a></span>
+<!--
+        <span class="alignleft"><?php previous_post('%', 'PREVIOUS', 'no'); ?></span>
+        <span class="alignright"><?php next_post('%', 'NEXT', 'no'); ?></span>
+ -->
+    </div>
 </section>
 
 
 <section id="about" class="section">
-	<?php echo get_avatar( get_the_author_email(), '84' ); ?>
-	<strong>About the Author</strong>
-	<p><?php the_author_meta('description'); ?></p>
+    <?php echo get_avatar( get_the_author_email(), '84' ); ?>
+    <strong>About the Author</strong>
+    <p><?php the_author_meta('description'); ?></p>
 </section>
 
 
 <section id="ads2" class="section">
-	<a href="#" class="ads2"></a>
+    <a href="#" class="ads2"></a>
 </section>
 
 
 
 <section id="comments" class="section">
-	<h3>Comments</h3>
-	<div id="comwrap">
-		<?php comments_template(); ?>
-	</div>
+    <h3>Comments</h3>
+    <div id="comwrap">
+        <?php comments_template(); ?>
+    </div>
 </section>
 
 
-	
-	
+    
+    
 <?php endwhile; ?>
 
 <div class="navigation">
-	<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
-	<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+    <div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
+    <div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
 </div>
 
 <?php else : ?>
