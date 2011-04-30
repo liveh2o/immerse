c6b802055b27c15cf8764c8d614d7d773825cca4
Adam Hutchison <liveh2o@gmail.com>
2011-04-30 06:09:16 +0000

    Changed the mode

---
+++ wp-content/themes/theme/scripts/paginator.js
@@ -0,0 +1,59 @@
+var position = 0;
+var offset = 705;
+var width = 0;
+var pages = 0;
+var current_page = 1;
+
+
+function nextPage() {
+  position = parseInt($(".paginate").css("margin-left")) - offset;
+  $(".paginate:first").css("margin-left",position+'px'); 
+  $(".paginate:first").effect("highlight", {color: "#efefef"}, 1000);
+  current_page += 1;
+  console.log("current page: " + current_page);
+  toggleNextButton();
+  togglePreviousButton();
+}
+
+function previousPage() {
+  position = parseInt($(".paginate").css("margin-left")) + offset;
+  $(".paginate:first").css("margin-left",position+'px'); 
+  $(".paginate:first").effect("highlight", {color: "#efefef"}, 1000);
+  current_page -= 1;
+  console.log("current page: " + current_page);
+  toggleNextButton();
+  togglePreviousButton();
+}
+
+function toggleNextButton() {
+  if (current_page == pages) {
+    $('#next_button').hide();
+  } else {
+    $('#next_button').show();
+  }
+}
+
+function togglePreviousButton() {
+  if (current_page == 1) {
+    $('#previous_button').hide();
+  } else {
+    $('#previous_button').show();
+  }
+}
+
+$(document).ready(function() {
+  var last_element = $('.paginate > p:last');
+  width = last_element.offset().left + last_element.width();
+  $(".paginate").css("width", width);
+  pages = parseInt(width / offset);
+  console.log("pages: " + pages);
+
+  $('#next_button').click(function(e) {
+    nextPage();
+    e.preventDefault();
+  });
+  $('#previous_button').click(function(e) {
+    previousPage();
+    e.preventDefault();
+  });
+});
