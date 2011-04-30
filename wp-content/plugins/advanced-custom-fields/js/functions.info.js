a30c1337e509da9680cfdf66db78fc8505d1fb89
Adam Hutchison <liveh2o@gmail.com>
2011-04-30 06:06:50 +0000

    Removing the advanced-custom-fields plugin

--- wp-content/plugins/advanced-custom-fields/js/functions.info.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-	
-	$('.wrap').wrapInner('<div class="cfm_col_left" />');
-	$('.wrap').wrapInner('<div class="cfm_cols" />');
-	//$('.cfm_col_left div.clear').remove();
-	$('.cfm_col_right').removeClass('hidden').prependTo('.cfm_cols');
-	
-});
-
