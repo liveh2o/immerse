1d1e7abb0d6cd866c34e6ab0a1ccbc8337ab6848
Adam Hutchison <liveh2o@gmail.com>
2011-05-01 03:12:51 +0000

    Lit up the advisory board views

--- wp-content/themes/theme/the-board.php
+++ wp-content/themes/theme/the-board.php
@@ -1,9 +1,7 @@
 <section id="extra" class="section">
 	<h4 id="board-title">The Board</h4>
-	
 	<ul id="board">
-		
-		<?php $f_loop = new WP_Query(); ?>
+		<?php $f_loop = new WP_Query('post_type=board&posts_per_page=-1'); ?>
 		<?php while ($f_loop->have_posts()) : $f_loop->the_post(); ?>	
 		<li>
 			<?php if ( has_post_thumbnail() ) {?>
@@ -19,8 +17,6 @@
 			<p><?php echo $employer; ?></p>
 			<p><?php echo $location; ?></p>
 		</li>
-		
 		<?php endwhile; ?>
-		
 	</ul>
 </section>	
