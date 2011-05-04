a47b5caa74f5bdb1359b5162598c489ca09d9ae3
Adam Hutchison <liveh2o@gmail.com>
2011-05-04 04:12:06 +0000

    Added th relevanssi search plugin and lit up the search

--- wp-content/themes/theme/search.php
+++ wp-content/themes/theme/search.php
@@ -6,8 +6,7 @@
 
 get_header();
 		
-include_once 'section-menu.php';
-query_posts($query_string . '&numberposts=-1'); ?>
+include_once 'section-menu.php'; ?>
 
 <h2 class="page-title">Search Results</h2>
 <section id="page" class="section search">
@@ -16,12 +15,17 @@ query_posts($query_string . '&numberposts=-1'); ?>
 	<?php while (have_posts()) : the_post(); ?>
 	<div class="entry is-post" id="post-<?php the_ID(); ?>">
 		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-		<p><?php the_excerpt(); ?></p>
+		<p><?php relevanssi_the_excerpt(); ?></p>
         <?php if (get_the_category()) { ?>
         <p><em>From <?php the_category(' '); ?></em></p>
         <?php } ?>
 	</div>
 	<?php endwhile; ?>
+
+    <div class="navigation">
+        <div class="alignleft"><?php previous_posts_link('PREVIOUS') ?></div>
+        <div class="alignright"><?php next_posts_link('NEXT') ?></div>
+    </div>
 
 </section>	
 
