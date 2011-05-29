2fc2f7d132a673a2493b92a1ad458d119ee0d8a3
Adam Hutchison <liveh2o@gmail.com>
2011-05-28 22:24:17 -0500

    Tweaked the title of the past articles popup

--- wp-content/themes/theme/single.php
+++ wp-content/themes/theme/single.php
@@ -47,7 +47,7 @@ get_header(); ?>
         'tt', 'Post to Twitter'); ?>
     </span>
 </section>
-<div id="past-articles-popup" title="Past <?php echo $category->cat_name; ?> Articles">
+<div id="past-articles-popup" title="Past <?php echo $category[0]->cat_name; ?> Articles">
     <?php past_articles(); ?>
 </div>
 
